279581c71144bd5af6a0d07e6d9498b373f1d2b3
MRSAMLEY <hunterproducer625@gmail.com>
2025-07-07 15:24:15 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 //login with id and user
 const users = [
+  { username: "Sokpheng", id: "123@sokphengict" },
+  { username: "Boromey", id: "123@sokphengict" },
+  { username: "Peaktra", id: "123@sokphengict" },
+  { username: "Saem", id: "123@sokphengict" },
+  { username: "Sangyuth", id: "123@sokphengict" },
+  { username: "Channary", id: "123@sokphengict" },
+  { username: "Sreypenh", id: "123@sokphengict" },
+  { username: "Kimleang", id: "123@sokphengict" },
+  { username: "Sarorng", id: "123@sokphengict" },
+  { username: "Sreyleav", id: "123@sokphengict" },
+  { username: "Vuthysony", id: "123@sokphengict" },
+  { username: "Somapeavy", id: "123@sokphengict" },
+  { username: "Sela", id: "123@sokphengict" },
+  { username: "Nary", id: "123@sokphengict" },
+  { username: "Channa", id: "123@sokphengict" },
+  { username: "Visal", id: "123@sokphengict" },
+  { username: "Molina", id: "123@sokphengict" },
+  { username: "Sovanrachana", id: "123@sokphengict" },
+  { username: "Thida", id: "123@sokphengict" },
   { username: "Mrsamley", id: "123@mrsamley" },
   // Add more from your sheet
 ];
@@ -26,7 +45,7 @@ function login() {
 function handleCredentialResponse(response) {
   const user = decodeJwt(response.credential);
   console.log("Logged in as:", user.email, user.name);
-  alert(`Logged in as: ${user.name} (${user.email})`);
+  alert(Logged in as: ${user.name} (${user.email}));
   // Optional: Save to sessionStorage
   sessionStorage.setItem("email", user.email);
   sessionStorage.setItem("name", user.name);
